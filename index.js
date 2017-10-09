0431039dfc2911992ecdf55859e3ef36363e8c0b
github username <logemerson@gmail.com>
2017-10-09 09:13:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function calculateVertical(block1, block2) {
+  return block1 > block2 ? (block1 - block2) * 264 : (block2 - block1) * 264;
+}
+
+function distanceFromHqInBlocks(block) {
+  return block > 42 ? block - 42 : 42 - block;
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(block1, block2) {
+  return calculateVertical(block1, block2);
+}
+
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * distance;
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
